cc5f6215aa6da9160d7a2be2b27bcd50ef474722
c-lei-en <chenglei713@gmail.com>
2021-01-04 16:23:37 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+var Cesium = require("cesium/Cesium");
+var widgets = require("cesium/Widgets/widgets.css");
+
+Vue.prototype.Cesium = Cesium;
+Vue.prototype.widgets = widgets;
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div id="cesiumContainer"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  mounted() {
+    let viewer = new this.Cesium.Viewer("cesiumContainer");
   }
 };
 </script>
